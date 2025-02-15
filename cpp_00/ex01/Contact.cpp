30c392603b6a629ebbc661d4d257892a403ab22b
Abdelali <abde69436@gmail.com>
2025-02-15 20:49:43 +0100

    contact

--- cpp_00/ex01/Contact.cpp
+++ cpp_00/ex01/Contact.cpp
@@ -1,19 +1,28 @@
 #include "Contact.hpp"
 
-Contact::Contact(std::string first_name, std::string last_name, std::string nickname,
-				 std::string phone_number, std::string darkest_secret) {
-	this->first_name = first_name;
-	this->last_name = last_name;
-	this->nickname = nickname;
-	this->phone_number = phone_number;
-	this->darkest_secret = darkest_secret;
+void	Contact::set_field(std::string type, std::string field) {
+	if (type == "first_name")
+		first_name = field;
+	else if (type == "last_name")
+		last_name = field;
+	else if (type == "nickname")
+		nickname = field;
+	else if (type == "phone")
+		phone_number = field;
+	else if (type == "secret")
+		darkest_secret = field;
 }
 
-Contact::~Contact( void ) {
-	std::cout << "Destructor Called" << std::endl;
+std::string	Contact::get_field(std::string type) {
+	if (type == "first_name")
+		return first_name;
+	else if (type == "last_name")
+		return last_name;
+	else if (type == "nickname")
+		return nickname;
+	else if (type == "phone")
+		return phone_number;
+	else if (type == "secret")
+		return darkest_secret;
+	return "Kl3z";
 }
-
-/* int main() {
-	Contact contact("ali", "mahdf", "klez", "324234", "dhsfilds");
-	std::cout << contact.first_name << contact.last_name<< contact.nickname<< contact.phone_number << std::endl;
-} */
--- cpp_00/ex01/Contact.hpp
+++ cpp_00/ex01/Contact.hpp
@@ -6,16 +6,14 @@
 
 class Contact {
 	private:
-		std::string	darkest_secret;
-	public:
 		std::string	first_name;
 		std::string	last_name;
 		std::string	nickname;
 		std::string	phone_number;
-	
-	Contact(std::string first_name, std::string last_name, std::string nickname,
-			std::string phone_number, std::string darkest_secret);
-	~Contact( void );
+		std::string	darkest_secret;
+	public:
+		void		set_field(std::string type, std::string field);
+		std::string	get_field(std::string type);
 };
 
 #endif
