9f2cee9ee7603447e183deeba64a45fa39109f89
Abdelali <abde69436@gmail.com>
2025-02-24 11:28:12 +0000

    cpp00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,10 +6,8 @@ int	main(int ac, char **av) {
 	if (ac > 1) {
 		for (int i = 1; i < ac ; i++) {
 			char *str = av[i];
-			for (size_t j = 0; j < std::strlen(str); j++) {
-				if (std::isalpha(str[j]))
-					str[j] = std::toupper(str[j]);
-			}
+			for (size_t j = 0; j < std::strlen(str); j++)
+				str[j] = std::toupper(str[j]);
 			std::cout << str;
 		}
 		std::cout << std::endl;
