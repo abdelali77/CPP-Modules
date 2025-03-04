7065fb8e19c9d424e8027c2f09092190608be3e0
Abdelali <abde69436@gmail.com>
2025-03-04 17:24:25 +0000

    ex01

---
+++ cpp_02/ex01/Fixed.cpp
@@ -0,0 +1,54 @@
+#include "Fixed.hpp"
+
+void Fixed::setRawBits( int const raw ) {
+	fixedValue = raw;
+}
+
+int	Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return fixedValue;
+}
+
+float Fixed::toFloat( void ) const {
+	return (float)fixedValue / (1 << fractional);
+}
+
+int Fixed::toInt( void ) const {
+	return fixedValue >> fractional;
+}
+
+Fixed::Fixed( const int n ) {
+	std::cout << "Int constructor called" << std::endl;
+	fixedValue = n << fractional;
+}
+
+Fixed::Fixed( const float n ) {
+	std::cout << "Float constructor called" << std::endl;
+	fixedValue = roundf(n * (1 << fractional));
+}
+
+Fixed::Fixed( void ) {
+	fixedValue = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const Fixed& fixed) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed& Fixed::operator=(const Fixed &fixed) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &fixed)
+		fixedValue = fixed.fixedValue;
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed) {
+	os << fixed.toFloat();
+	return os;
+}
+
+Fixed::~Fixed( void ) {
+	std::cout << "Destructor called" << std::endl;
+}
---
+++ cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef __FIXED_HPP__
+#define __FIXED_HPP__
+
+#include <iostream>
+#include <cmath>
+
+// ************************************************************************** //
+//                                 Fixed Class                                //
+// ************************************************************************** //
+
+class Fixed {
+private:
+	int	fixedValue;
+	static const int fractional = 8;
+public:
+	int	getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	Fixed( const int n );
+	Fixed( const float n );
+	Fixed( void );
+	Fixed( const Fixed& fixed);
+	Fixed& operator=(const Fixed &fixed);
+	~Fixed();
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed);
+
+#endif
