43265e147af2ef0ef4c107732df45b093bf86613
Abdelali <abde69436@gmail.com>
2025-03-05 13:48:50 +0000

    ex02

---
+++ cpp_02/ex02/Fixed.cpp
@@ -0,0 +1,123 @@
+#include "Fixed.hpp"
+
+void Fixed::setRawBits( int const raw ) {
+	fixedValue = raw;
+}
+
+int	Fixed::getRawBits( void ) const {
+	return fixedValue;
+}
+
+float Fixed::toFloat( void ) const {
+	return (float)fixedValue / (1 << fractional);
+}
+
+int Fixed::toInt( void ) const {
+	return fixedValue >> fractional;
+}
+
+Fixed::Fixed( const int n ) {
+	fixedValue = n << fractional;
+}
+
+Fixed::Fixed( const float n ) {
+	fixedValue = roundf(n * (1 << fractional));
+}
+
+Fixed::Fixed( void ) : fixedValue(0) {}
+
+Fixed::Fixed( const Fixed& fixed) {
+	*this = fixed;
+}
+
+Fixed& Fixed::operator=(const Fixed &fixed) {
+	if (this != &fixed)
+		fixedValue = fixed.fixedValue;
+	return *this;
+}
+
+bool Fixed::operator>(const Fixed &fixed) const {
+	return fixedValue > fixed.fixedValue;
+}
+
+bool Fixed::operator<(const Fixed &fixed) const {
+	return fixedValue < fixed.fixedValue;
+}
+
+bool Fixed::operator>=(const Fixed &fixed) const {
+	return fixedValue >= fixed.fixedValue;
+}
+
+bool Fixed::operator<=(const Fixed &fixed) const {
+	return fixedValue <= fixed.fixedValue;
+}
+
+bool Fixed::operator==(const Fixed &fixed) const {
+	return fixedValue == fixed.fixedValue;
+}
+
+bool Fixed::operator!=(const Fixed &fixed) const {
+	return fixedValue != fixed.fixedValue;
+}
+
+Fixed Fixed::operator+(const Fixed &fixed) {
+	return fixedValue + fixed.fixedValue;
+}
+
+Fixed Fixed::operator-(const Fixed &fixed) {
+	return fixedValue - fixed.fixedValue;
+}
+
+Fixed Fixed::operator*(const Fixed &fixed) {
+	return this->toFloat() * fixed.toFloat();
+}
+
+Fixed Fixed::operator/(const Fixed &fixed) {
+	return this->toFloat() / fixed.toFloat();
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed tmp(*this);
+	this->fixedValue++;
+	return tmp;
+}
+
+Fixed Fixed::operator++(void) {
+	this->fixedValue++;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed tmp(*this);
+	this->fixedValue--;
+	return tmp;
+	
+}
+
+Fixed Fixed::operator--(void) {
+	this->fixedValue--;
+	return *this;
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b) {
+	return a < b ? a : b;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b) {
+	return a < b ? a : b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b) {
+	return a > b ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
+	return a > b ? a : b;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed) {
+	os << fixed.toFloat();
+	return os;
+}
+
+Fixed::~Fixed( void ) {}
---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,48 @@
+#ifndef __FIXED_HPP__
+#define __FIXED_HPP__
+
+#include <iostream>
+#include <cmath>
+
+// ************************************************************************** //
+//                                 Fixed Class                                //
+// ************************************************************************** //
+
+class Fixed {
+private:
+	int	fixedValue;
+	static const int fractional = 8;
+public:
+	int	getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	Fixed( const int n );
+	Fixed( const float n );
+	Fixed( void );
+	Fixed( const Fixed& fixed);
+	Fixed& operator=(const Fixed &fixed);
+	bool operator>(const Fixed &fixed) const;
+	bool operator<(const Fixed &fixed) const;
+	bool operator>=(const Fixed &fixed) const;
+	bool operator<=(const Fixed &fixed) const;
+	bool operator==(const Fixed &fixed) const;
+	bool operator!=(const Fixed &fixed) const;
+	Fixed operator+(const Fixed &fixed);
+	Fixed operator-(const Fixed &fixed);
+	Fixed operator*(const Fixed &fixed);
+	Fixed operator/(const Fixed &fixed);
+	Fixed operator++(int);
+	Fixed operator++(void);
+	Fixed operator--(int);
+	Fixed operator--(void);
+	static Fixed& min(Fixed& a, Fixed& b);
+	static const Fixed& min(const Fixed& a, const Fixed& b);
+	static Fixed& max(Fixed& a, Fixed& b);
+	static const Fixed& max(const Fixed& a, const Fixed& b);
+	~Fixed();
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed &fixed);
+
+#endif
