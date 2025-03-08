b54a14aaf1e75e86504e49922c21c35dfb3fdd34
Abdelali <abde69436@gmail.com>
2025-03-08 11:24:55 +0000

    ex01

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -1,7 +1,11 @@
 #include "Zombie.hpp"
 
 int	main() {
-	Zombie	*zombies = zombieHorde(4, "foo");
+	int N = 5;
+	Zombie	*zombies = zombieHorde(N, "foo");
+	for (int i = 0; i < N; i++) {
+		zombies[i].announce();
+	}
 	delete[] zombies;
 
 	return 0;
