afb6996599960563aaad13f29fe5fb95f8074256
Abdelali <abde69436@gmail.com>
2025-04-09 10:11:51 +0100

    ex02

--- cpp_03/ex02/main.cpp
+++ cpp_03/ex02/main.cpp
@@ -1,11 +1,11 @@
 #include "FragTrap.hpp"
 
 int main( void ) {
-	FragTrap su("SU-57");
+	FragTrap su("su-57");
 
 	su.takeDamage(10);
 	su.highFivesGuys();
-	su.attack("F-35");
+	su.attack("f-35");
 	su.beRepaired(10);
 
 	return 0;
