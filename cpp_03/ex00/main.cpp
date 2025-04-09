bb07432f25d427afb5dd1a6cd904d52ee656e937
Abdelali <abde69436@gmail.com>
2025-04-09 10:11:43 +0100

    ex00

--- cpp_03/ex00/main.cpp
+++ cpp_03/ex00/main.cpp
@@ -1,14 +1,14 @@
 #include "ClapTrap.hpp"
 
 int main( void ) {
-	ClapTrap f_35("f-35");
-	ClapTrap su_57("su-57");
+	ClapTrap clap1("f-35");
+	ClapTrap clap2("su-57");
 
-	su_57.attack("f-35");
-	f_35.takeDamage(40);
-	f_35.attack("su-57");
-	su_57.takeDamage(5);
-	su_57.beRepaired(20);
+	clap2.attack("f-35");
+	clap1.takeDamage(40);
+	clap1.attack("su-57");
+	clap2.takeDamage(5);
+	clap2.beRepaired(20);
 
 	return 0;
 }
