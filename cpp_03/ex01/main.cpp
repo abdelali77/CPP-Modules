1b7772a73cb786f2d210d5a05ce9919434791856
Abdelali <abde69436@gmail.com>
2025-04-09 10:11:35 +0100

    ex01

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -1,11 +1,11 @@
 #include "ScavTrap.hpp"
 
 int main( void ) {
-	ScavTrap su("SU-57");
+	ScavTrap su("su-57");
 
 	su.takeDamage(10);
 	su.guardGate();
-	su.attack("F-35");
+	su.attack("f-35");
 	su.beRepaired(10);
 
 	return 0;
