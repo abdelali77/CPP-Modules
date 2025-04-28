915b9d52e175c33b75c5393f59ccfaa0f8b06734
Abdelali <abde69436@gmail.com>
2025-04-28 10:31:12 +0100

    ex00

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -2,9 +2,12 @@
 
 int main( void ) {
 	try {
-		Bureaucrat b("Saddam", 150);
+		Bureaucrat b("Saddam", 1);
 		std::cout << b;
-		b.decrementGrade();
+		Bureaucrat d("Kadafi", 150);
+		std::cout << d;
+		b.incrementGrade();
+		d.decrementGrade();
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
