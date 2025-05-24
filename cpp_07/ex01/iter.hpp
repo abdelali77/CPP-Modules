61bad75f0b7cecf3e67d29dad811f7c3b4a26a8e
Abdelali <abde69436@gmail.com>
2025-05-24 10:18:37 +0100

    ex01

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -4,12 +4,20 @@
 #include <iostream>
 #include <string>
 
-template <typename ARR, typename FUNC>
-void iter(ARR *arr, size_t len, FUNC func) {
+template <typename ARR>
+void iter(ARR *arr, size_t len, void(*f)(ARR&)) {
 	if (!arr || len == 0)
 		return;
 	for (size_t i = 0; i < len; i++)
-		func(arr[i]);
+		f(arr[i]);
+}
+
+template <typename ARR>
+void iter(const ARR *arr, size_t len, void(*f)(const ARR&)) {
+	if (!arr || len == 0)
+		return;
+	for (size_t i = 0; i < len; i++)
+		f(arr[i]);
 }
 
 #endif
