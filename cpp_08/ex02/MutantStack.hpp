9d935bf81aced4f5216a54e93fb5b677a284de57
Kl3z <78610663+abdelali77@users.noreply.github.com>
2025-06-21 10:51:07 +0000

    ex02

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -12,17 +12,23 @@
 
 template <typename T>
 
-class MutantStack : public std::stack<T> {
+class MutantStack : public std::stack<T, std::deque<T> > {
 public:
 	MutantStack( void ) { }
-	MutantStack( const MutantStack& oth ) { (void)oth; }
+	MutantStack( const MutantStack& oth ) : std::stack<T>(oth) { (void)oth; }
 	MutantStack& operator=( const MutantStack& oth ) {
 		std::stack<T>::operator=(oth);
+		return *this;
 	}
+
 	typedef typename std::deque<T>::iterator iterator;
+	typedef typename std::deque<T>::const_iterator const_iterator;
 
 	iterator begin() { return this->c.begin(); }
 	iterator end() { return this->c.end(); }
+
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
 
 	~MutantStack() { }
 };
