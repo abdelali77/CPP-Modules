ee1a2199f60c478396a700b81a92f364110f94c9
Abdelali <abde69436@gmail.com>
2025-09-13 10:54:10 +0100

    ex02

---
+++ cpp_09/ex02/PmergeMe.hpp
@@ -0,0 +1,19 @@
+#ifndef __PMERGEME_HPP__
+#define __PMERGEME_HPP__
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <deque>
+
+// ************************************************************************** //
+//                               PmergeMe Class                               //
+// ************************************************************************** //
+
+class PmergeMe {
+private:
+	std::vector<int> vec;
+};
+
+#endif
