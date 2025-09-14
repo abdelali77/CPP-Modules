3049a432a41aa081f06c186cc11e8e170bf7911a
Kl3z <78610663+abdelali77@users.noreply.github.com>
2025-09-14 17:23:13 +0000

    main

--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -1,1 +1,15 @@
 #include "PmergeMe.hpp"
+
+int main( int ac, char **av ) {
+	if (ac == 1) {
+		std::cerr << "Usage: ./PmergeMe [...]" << std::endl;
+		return 1;
+	} else {
+		if (parseInput(ac, av)) {
+			PmergeMe::init_data(ac, av);
+		} else {
+			std::cerr << "Error" << std::endl;
+		}
+	}
+	return 0;
+}
