5979dd52b063d32a70c4fd14499008b9737d1f04
Abdelali <abde69436@gmail.com>
2025-09-25 15:10:29 +0100

    ex00

--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -11,22 +11,4 @@ int main( int ac, char **av ) {
 	} else {
 		std::cerr << "Usage: ./btc [file]" << std::endl;
 	}
-
-	/* std::map<std::string, int>	_map;
-
-	_map["01"] = 1;
-	_map["02"] = 2;
-	_map["03"] = 3;
-	// _map["04"] = 4;
-	_map["05"] = 5;
-
-	std::map<std::string, int>::iterator it = _map.begin();
-	while (it != _map.end())
-	{
-		std::cout << it->first << " " << it->second << std::endl;
-		it++;
-	}
-	if (_map.lower_bound("06") == _map.end())
-		std::cout << "  +++++++++++++++ "<< std::endl;
-	return 0; */
 }
